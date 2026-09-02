49399ee7aa73596a2087586fb9af520c78ccb3e0
GodRishi <saharishi409@gmail.com>
2026-09-02 22:19:12 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   server: {
@@ -9,6 +10,16 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: true,
     target: 'es2015',
-    cssTarget: 'safari12'
+    cssTarget: 'safari12',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, '404.html'),
+        500: resolve(__dirname, '500.html'),
+        privacy: resolve(__dirname, 'privacy-policy.html'),
+        terms: resolve(__dirname, 'terms-of-service.html'),
+        cookie: resolve(__dirname, 'cookie-policy.html')
+      }
+    }
   }
 });
